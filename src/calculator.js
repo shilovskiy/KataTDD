14946ca9be28673ee156fb255eea44ed45b7ccb8
shilovskiy <shilovskiy@gmail.com>
2017-01-25 09:18:55 +0300

    MultNumberInString

--- src/calculator.js
+++ src/calculator.js
@@ -3,29 +3,31 @@
  */
 
 
-class Calculator{
-    constructor(){
+class Calculator {
+    constructor() {
         this.defaultValue = 0;
     }
 
+    calculateSumm(str) {
+        var digits = str.split(',');
+        return digits.reduce((total, d) => {
+            return parseInt(total) + parseInt(d);
+        });
+    }
 
-    summ(str){
+    summ(str) {
         var result;
 
-        if (str.length==1){
+
+        if (str.length == 1) {
             result = parseInt(str);
 
-        }else{
-            if (str.indexOf(',')>0){
-            var digits = str.split(',');
-
-            result = digits.reduce((total,d)=>{
-                return parseInt(total)+parseInt(d);
-            });
-                //parseInt(parseInt(digits[0])+parseInt(digits[1]));
-        }else{
-            result = this.defaultValue
-        }
+        } else {
+            if (str.indexOf(',') > 0) {
+                result = this.calculateSumm(str);
+            } else {
+                result = this.defaultValue
+            }
         }
         return result;
     }
